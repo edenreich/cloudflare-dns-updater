104168ebf11e1fa94e9f667a19bcf5a645cff3a9
Eden <eden.reich@gmail.com>
2022-04-17 23:48:17 +0200

    Some refactoring, use K8S environment variable to decide if this binary is going to be used as a controller

--- src/cloudflare.rs
+++ src/cloudflare.rs
@@ -4,11 +4,10 @@ extern crate hyper_tls;
 extern crate serde;
 extern crate serde_json;
 
-use std::{thread};
-use hyper::Client;
-use hyper_tls::HttpsConnector;
-use hyper::{Request, Body};
+use std::{thread, env};
 use clap::{Arg, App, SubCommand, AppSettings};
+use hyper::{Client, Request, Body};
+use hyper_tls::HttpsConnector;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -36,16 +35,15 @@ struct CloudflareDNSListResponse {
     messages: Vec<String>,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let matches = App::new("Cloudflare DNS Updater")
+fn cli() -> App<'static, 'static> {
+        App::new("Cloudflare DNS Updater")
         .version("1.0")
         .author("Eden Reich <eden.reich@gmail.com>")
         .about("Update a DNS records on Cloudflare with dynamic public IP-Address")
         .usage("cloudflare update --token [ACCESS_TOKEN] --zone [ZONE_ID] --dns [DNS_LIST..]")
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .subcommand(SubCommand::with_name("update")
-            .about("Updates a list of DNS with public ip-address")
+            .about("Updates a list of DNS with public IP-Address")
             .arg(Arg::with_name("dns")
                 .short("d")
                 .long("dns")
@@ -77,12 +75,43 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 .default_value("2")
                 .value_name("INTERVALS")
                 .help("How often to check in seconds")))
-        .get_matches();
+}
+
+async fn get_ip_address() -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
+    let geoip_api_endpoint: String = "https://checkip.amazonaws.com".to_owned();
+    let https = HttpsConnector::new();
+    let client = Client::builder().build::<_, hyper::Body>(https);
+    let ip_address_request = Request::builder()
+    .method("GET")
+    .uri(&geoip_api_endpoint)
+    .header("Content-Type", "application/json")
+    .body(Body::empty())
+    .expect("request builder to return ip address");
+
+    let ip_address_raw_response = client.request(ip_address_request).await?;
+
+    if !ip_address_raw_response.status().is_success() {
+        panic!("failed to get a successful response of your public ip address!");
+    }
+
+    let ip_response_content = hyper::body::to_bytes(ip_address_raw_response).await?;
+
+    Ok(std::str::from_utf8(&ip_response_content).unwrap().to_owned())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    if env::var("K8S").is_ok() {
+        // run the reconciling kubernetes loop
+        return Ok(());
+    }
+
+    let matches = cli().get_matches();
 
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
-    let mut ip_address: String = "127.0.0.1".to_owned();
-    let geoip_api_endpoint: String = "https://checkip.amazonaws.com".to_owned();
+    let mut current_ip_address: String = "127.0.0.1".to_owned();
+    
     let update_command = matches.subcommand_matches("update").unwrap();
 
     let intervals: u64 = update_command.value_of("intervals").unwrap().parse::<u64>()?;
@@ -92,29 +121,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let cloudflare_api_dns_endpoint: String = format!("https://api.cloudflare.com/client/v4/zones/{}/dns_records", input_cloudflare_zone_id);
 
     loop {
-
-        let ip_address_request = Request::builder()
-            .method("GET")
-            .uri(&geoip_api_endpoint)
-            .header("Content-Type", "application/json")
-            .body(Body::empty())
-            .expect("request builder to return ip address");
-
-        let ip_address_raw_response = client.request(ip_address_request).await?;
-
-        if !ip_address_raw_response.status().is_success() {
-            panic!("failed to get a successful response of your public ip address!");
-        }
-
-        let ip_response_content = hyper::body::to_bytes(ip_address_raw_response).await?;
-
-        thread::sleep(std::time::Duration::from_secs(intervals));
-
-        if ip_response_content == ip_address {
+        let ip_address = get_ip_address().await.unwrap();
+        if current_ip_address == ip_address {
+            println!("IP address is the same, skipping update");
+            thread::sleep(std::time::Duration::from_secs(intervals));
             continue;
         }
-        
-        ip_address = std::str::from_utf8(&ip_response_content).unwrap().to_owned();
+        current_ip_address = ip_address;
 
         let dns_list_request = Request::builder()
             .method("GET")
@@ -150,7 +163,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                     id: dns.id.to_owned(),
                     r#type: dns.r#type.to_owned(), 
                     name: input_dns.to_owned(), 
-                    content: ip_address.to_owned(), 
+                    content: current_ip_address.to_owned(), 
                     proxied: true 
                 }).unwrap();
 
@@ -165,7 +178,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 let dns_update_raw_response = client.request(dns_update_request).await?;
 
                 if !dns_update_raw_response.status().is_success() {
-                    panic!(format!("failed to update the dns {} records on cloudflare!", input_dns));
+                    panic!("failed to update the dns {} records on cloudflare!", input_dns);
                 }
         
                 let dns_update_response_content_bytes = hyper::body::to_bytes(dns_update_raw_response).await?;
@@ -173,13 +186,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 let dns_update_response: CloudflareDNSResponse = serde_json::from_str::<CloudflareDNSResponse>(&dns_update_response_content).unwrap();
 
                 if dns_update_response.success == true {
-                    println!("DNS {} was assigned with the following ip {} successfully", dns.name, ip_address);
+                    println!("DNS {} was assigned with the following ip {} successfully", dns.name, current_ip_address);
                 }
 
                 break;
             }
         }
     }
-
-    Ok(())
 }
